24f3328e2e82ab6b47850f3f040b590b93d793cb
Jean Marchand <jeanno11@orange.fr>
2024-04-25 09:49:03 +0200

    Revert changes on fee

--- programs/network/src/state/fee.rs
+++ programs/network/src/state/fee.rs
@@ -18,20 +18,22 @@ impl Fee {
     pub fn pubkey(worker: Pubkey) -> Pubkey {
         Pubkey::find_program_address(&[SEED_FEE, worker.as_ref()], &crate::ID).0
     }
-
-    /// Derive the pubkey of a fee account.
-    pub fn key(&self) -> Pubkey {
-        Fee::pubkey(self.worker)
-    }
 }
 
 /// Trait for reading and writing to a fee account.
 pub trait FeeAccount {
+    /// Get the pubkey of the fee account.
+    fn pubkey(&self) -> Pubkey;
+
     /// Initialize the account to hold fee object.
     fn init(&mut self, worker: Pubkey, bump: u8) -> Result<()>;
 }
 
 impl FeeAccount for Account<'_, Fee> {
+    fn pubkey(&self) -> Pubkey {
+        Fee::pubkey(self.worker)
+    }
+
     fn init(&mut self, worker: Pubkey, bump: u8) -> Result<()> {
         self.distributable_balance = 0;
         self.worker = worker;
